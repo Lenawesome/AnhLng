1d2d9a936cebcab5fe2285ff0ad7c9de620cab12
Lenawesome <nawilldominate@gmail.com>
2019-07-30 10:00:32 +0700

    Use customer model to changepassword

--- ChangePW/Controller/Adminhtml/Index/Save.php
+++ ChangePW/Controller/Adminhtml/Index/Save.php
@@ -9,18 +9,21 @@ class Save extends \Magento\Backend\App\Action
     protected $customerRepository;
     protected $encryptor;
     protected $resultRedirect;
+    protected $customerFactory;
 
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
         \Magento\Framework\View\Result\PageFactory $resultPageFactory,
-        \Magento\Customer\Model\ResourceModel\CustomerRepository $customerRepository,
+//        \Magento\Customer\Model\ResourceModel\CustomerRepository $customerRepository,
+        \Magento\Customer\Model\CustomerFactory $customerFactory,
         \Magento\Framework\Encryption\EncryptorInterface $encryptor,
         \Magento\Framework\Controller\ResultFactory $result
     )
     {
+        $this->customerFactory = $customerFactory;
         $this->resultPageFactory = $resultPageFactory;
         $this->resultRedirect = $result;
-        $this->customerRepository = $customerRepository;
+//        $this->customerRepository = $customerRepository;
         $this->encryptor = $encryptor;
         $this->resultRedirect = $result;
         parent::__construct($context);
@@ -33,8 +36,12 @@ class Save extends \Magento\Backend\App\Action
         $data = $this->getRequest()->getPostValue();
         $id = $data['entity_id'];
         $password = $data['password'];
-        $customer = $this->customerRepository->getById($id);
-        $this->customerRepository->save($customer, $this->encryptor->getHash($password, true));
+        $customer = $this->customerFactory->create();
+        $customer = $customer->load($id);
+        $customer = $customer->changePassword($password);
+        $customer->save();
+//        $customer = $this->customerRepository->getById($id);
+//        $this->customerRepository->save($customer, $this->encryptor->getHash($password, true));
         return $this->resultRedirect;
     }
 }
